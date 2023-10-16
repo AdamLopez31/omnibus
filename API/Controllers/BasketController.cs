deef9245c9c86caec640508c66110391b1c588db
Adam Lopez <adaml8978@gmail.com>
2023-10-15 19:56:13 -0500

    more work done

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -21,39 +21,49 @@ namespace API.Controllers
         //3 endpoints
         //1. to fetch individual basket
         [HttpGet]
-        public async Task<ActionResult<Basket>> GetBasket() {
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
             //USE COOKIE TO IDENTIFY BASKET
             //WHEN USER CREATES A BASKET ON SERVER A BUYERID WILL BE RETURNED SENT
             //BACK AS A COOKIE COOKIES STORE IN USER'S BROWSER IN STORAGE
             //FOR EVERY REQUEST AND RESPONSE WE USE THE COOKIE AND IT GOES BACKWARDS
             //AND FORWARDS BETWEEN CLIENT AND SERVER
-            var basket = await _context.Baskets
-            //EXPLICITLY RETURN BASKET ITEMS ENTITY FRAMEWORK WILL INCLUDE RELATED ITEMS WITH BASKET
-            .Include(i => i.Items)
-            .ThenInclude(p => p.Product)
-            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+            var basket = await RetrieveBasket();
 
-            if(basket == null) return NotFound();
+            if (basket == null) return NotFound();
 
             return basket;
         }
 
-
-
-
-        //2. add item to basket
-        [HttpPost]
+        //2. add item to basket use query string
+        [HttpPost] //api/basket?productId=3&quantity=2 ONE OF THE POWERS API CONTROLLER HAS ABLE TO READ QUERY STRING
+        //AS LONG AS KEYS IN QUERY STRING MATCHES NAMES IN PARAMETERS IT WILL KNOW THAT'S WHERE WE WANT TO GET THEM FROM
         public async Task<ActionResult> AddItemToBasket(int productId, int quantity) {
 
             //get basket or create basket
+            //RetrieveBasket method will either retrieve us a basket or it's going to 
+            //be null if FirstOrDefaultAsync method doesn't find what's it's looking for 
+            //in database it will return default value for an object: which is null
+            var basket = await RetrieveBasket();
+            if(basket == null) basket = CreateBasket();
 
             //get product
+            var product = await _context.Products.FindAsync(productId);
+            if(product == null) return NotFound();
 
             //add item
+            basket.AddItem(product,quantity);
 
             //save changes
-            return StatusCode(201);
+            //SaveChangesAsync returns integer of how many changes we made in database 
+            // > 0 we know something has happened to our database
+            var result = await _context.SaveChangesAsync() > 0;
+            if(result) return StatusCode(201);
+
+            return BadRequest(new ProblemDetails {Title = "Problem saving product to basket"});
         }
+
+        
 
 
         //3. remove item from basket or reduce quantity of basket
@@ -68,6 +78,37 @@ namespace API.Controllers
 
             //save changes
             return Ok();
+        }
+
+        private async Task<Basket> RetrieveBasket()
+        {
+            var basket = await _context.Baskets
+            //EXPLICITLY RETURN BASKET ITEMS ENTITY FRAMEWORK WILL INCLUDE RELATED ITEMS WITH BASKET
+            .Include(i => i.Items)
+            .ThenInclude(p => p.Product)
+            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+            return basket;
+        }
+
+        private Basket CreateBasket()
+        {
+            //add buyer ID randomly generated number globally unique identifier
+            var buyerId = Guid.NewGuid().ToString();
+            
+            //EUROPEAN POLICY ON COOKIES FORCE USERS TO ACCEPT COOKIES ON WEBSITE 
+            //SPECIFY COOKIE IS ESSENTIAL TO OPERATION OF WEBSITE
+            var cookieOptions = new CookieOptions {
+                IsEssential = true,
+                Expires = DateTime.Now.AddDays(30)
+            };
+            //BECAUSE WE'RE IN A CONTROLLER WE HAVE ACCESS TO THE HTTP RESPONSE WE'RE GOING TO SEND BACK
+            Response.Cookies.Append("buyerId",buyerId, cookieOptions);
+
+            var basket = new Basket{ BuyerId = buyerId};
+            //ENTITY FRAMEWORK WILL START TRACKING THIS NEW ENTITY WE'VE CREATED
+            _context.Baskets.Add(basket);
+
+            return basket;
         }
 
     }
