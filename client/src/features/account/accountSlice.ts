c877b3f4586432255801526bf781b02b17bec6a3
Adam Lopez <adaml8978@gmail.com>
2023-11-29 18:54:50 -0600

    more work done

---
+++ client/src/features/account/accountSlice.ts
@@ -0,0 +1,63 @@
+import { createAsyncThunk, createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { User } from "../../app/models/user";
+import { FieldValues } from "react-hook-form";
+import agent from "../../app/api/agent";
+
+interface AccountState {
+    user: User | null;
+}
+
+const initialState: AccountState = {
+    user: null
+}
+
+
+//createAsyncThunk must use extra reducers
+//LOGIN
+export const signInUser = createAsyncThunk<User,FieldValues>(
+    //type prefix
+    'account/signInUser',
+    async (data, thunkAPI) => {
+        try {
+            const user = await agent.Account.login(data);
+            localStorage.setItem('user',JSON.stringify(user));
+            return user;
+        } catch (error:any) {
+            return thunkAPI.rejectWithValue({error:error.data});
+        }
+    }
+);
+
+
+
+
+//FETCHING CURRENTLY LOGGED IN USER
+export const fetchCurrentUser = createAsyncThunk<User>(
+    //type prefix
+    'account/fetchUser',
+    async (_, thunkAPI) => {
+        try {
+            const user = await agent.Account.currentUser();
+            localStorage.setItem('user',JSON.stringify(user));
+            return user;
+        } catch (error:any) {
+            return thunkAPI.rejectWithValue({error:error.data});
+        }
+    }
+);
+
+export const accountSlice = createSlice({
+    name: 'account',
+    initialState,
+    reducers: {},
+    //use addMatcher because our 2 createAsyncThunk are both returning user and we 
+    //want to set our user in them
+    extraReducers: (builder => {
+        builder.addMatcher(isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled), (state,action) => {
+            state.user = action.payload;
+        });
+        builder.addMatcher(isAnyOf(signInUser.rejected, fetchCurrentUser.rejected), (_state,action) => {
+            console.log(action.payload);
+        }); 
+    })
+})
