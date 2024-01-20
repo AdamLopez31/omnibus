afc1fb14be002629c2de20172c099e27afad8449
Adam Lopez <adaml8978@gmail.com>
2024-01-20 12:27:21 -0600

    more work done

--- client/src/app/components/AppTextInput.tsx
+++ client/src/app/components/AppTextInput.tsx
@@ -3,12 +3,24 @@ import { UseControllerProps, useController } from "react-hook-form";
 
 interface Props extends UseControllerProps {
     label: string;
+    multiline?:boolean;
+    rows?:number;
+    type?:string;
 }
 
 export default function AppTextInput(props: Props) {
     const {fieldState,field} = useController({...props, defaultValue: ''})
     return (
-        <TextField {...props} {...field} fullWidth variant="outlined" error={!!fieldState.error} helperText={fieldState.error?.message}>
+        <TextField 
+        {...props} 
+        {...field} 
+        multiline={props.multiline} 
+        rows={props.rows}
+        type={props.type}
+        fullWidth 
+        variant="outlined" 
+        error={!!fieldState.error} 
+        helperText={fieldState.error?.message}>
             {/* !!fieldState.error !! casting to boolean */}
             {/* field onChange event name of text field onBlur event and its value */}
         </TextField>
--- client/src/features/admin/ProductForm.tsx
+++ client/src/features/admin/ProductForm.tsx
@@ -1,55 +1,66 @@
 import { Typography, Grid, Paper, Box, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import AppTextInput from "../../app/components/AppTextInput";
 import { Product } from "../../app/models/product";
 import { useEffect } from "react";
+import useProducts from "../../app/hooks/useProducts";
+import AppSelectList from "../../app/components/AppSelectList";
+import AppDropzone from "../../app/components/AppDropzone";
 
 interface Props {
     product?: Product;
     cancelEdit: () => void;
 }
 
-export default function ProductForm({product, cancelEdit} : Props) {
+export default function ProductForm({ product, cancelEdit }: Props) {
     //RESET FUNCTION IS FROM useForm
-    const { control, reset } = useForm();
+    const { control, reset, handleSubmit } = useForm();
+
+    const { brands, types } = useProducts();
 
     useEffect(() => {
         //IF WE HAVE PRODUCT RESET TO WHATEVER THE PRODUCT IS
-        if(product) reset(product);
+        if (product) reset(product);
+    }, [product, reset]);
 
-    },[product,reset]);
+    function handleSubmitData(data: FieldValues) {
+        console.log(data);
+
+    }
     return (
-        <Box component={Paper} sx={{p: 4}}>
-            <Typography variant="h4" gutterBottom sx={{mb: 4}}>
+        <Box component={Paper} sx={{ p: 4 }}>
+            <Typography variant="h4" gutterBottom sx={{ mb: 4 }}>
                 Product Details
             </Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={12}>
-                    <AppTextInput control={control} name='name' label='Product name' />
+            <form onSubmit={handleSubmit(handleSubmitData)}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={12}>
+                        <AppTextInput control={control} name='name' label='Product name' />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <AppSelectList items={brands} control={control} name='brand' label='Brand' />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <AppSelectList items={types} control={control} name='type' label='Type' />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <AppTextInput type='number' control={control} name='price' label='Price' />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <AppTextInput type='number' control={control} name='quantityInStock' label='Quantity in Stock' />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <AppTextInput multiline={true} rows={4} control={control} name='description' label='Description' />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <AppDropzone control={control} name='file' />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='brand' label='Brand' />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='type' label='Type' />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='price' label='Price' />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppTextInput control={control} name='quantityInStock' label='Quantity in Stock' />
-                </Grid>
-                <Grid item xs={12}>
-                    <AppTextInput control={control} name='description' label='Description' />
-                </Grid>
-                <Grid item xs={12}>
-                    <AppTextInput control={control} name='pictureUrl' label='Image' />
-                </Grid>
-            </Grid>
-            <Box display='flex' justifyContent='space-between' sx={{mt: 3}}>
-                <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
-                <Button variant='contained' color='success'>Submit</Button>
-            </Box>
+                <Box display='flex' justifyContent='space-between' sx={{ mt: 3 }}>
+                    <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
+                    <Button type="submit" variant='contained' color='success'>Submit</Button>
+                </Box>
+            </form>
         </Box>
     )
 }
